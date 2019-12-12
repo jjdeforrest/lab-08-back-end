30261a0ac90b63424b93264667455d9a5b54d2ea
gmireles210 <44076044+gmireles210@users.noreply.github.com>
2019-12-12 10:56:47 -0800

    updated js

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3001;
+const GEOCODE_API_Key = process.env.GOOGLE_API_KEY;
 const superagent = require('superagent');
 app.use(cors());
+
 const pg = require('pg');
 
 const client = new pg.Client(process.env.DATABASE_URL);
@@ -26,8 +28,8 @@ function FormattedData(query, location) {
 }
 
 app.get('/location', (request, response) => {
-  const search_query = request.query.data;
-  const urlToVisit = `https://maps.googleapis.com/maps/api/geocode/json?address=${search_query}&key=${process.env.GEOCODING_API_KEY}`;
+  const searchQuery = request.query.data;
+  const urlToVisit = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchQuery}&key=${process.env.GOOGLE_API_KEY}`;
 
   superagent.get(urlToVisit).then(responseFromSuper => {
     //console.log('stuff', responseFromSuper.body);
@@ -44,7 +46,6 @@ app.get('/location', (request, response) => {
   });
 })
 
-// WEATHER DATA
 
 /// WEATHER ////
 
@@ -55,7 +56,7 @@ function WeatherGetter(weatherValue) {
 app.get('/weather', (request, response) => {
   const weather_query = request.query.data
 
-  const urlToVisit = `https://api.darksky.net/forecast/${process.env.darksky}/${weather_query.latitude},${weather_query.longitude}`;
+  const urlToVisit = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${weather_query.latitude},${weather_query.longitude}`;
   superagent.get(urlToVisit).then(responseFromSuper => {
     //console.log('stuff', responseFromSuper.body);
     const darkskyData = responseFromSuper.body;
@@ -81,10 +82,8 @@ function Eventbrite(eventObj) {
 }
 app.get('/events', (request, response) => {
   const event_query = request.query.data
-
   const urlToVisit = `http://api.eventful.com/json/events/search?location=${event_query.formatted_query}&date=Future&app_key=${process.env.EVENT_API_KEY}`;
   console.log(urlToVisit);
-
   superagent.get(urlToVisit).then(responseFromSuper => {
     const body = JSON.parse(responseFromSuper.text);
     const events = body.events.event;
@@ -97,4 +96,5 @@ app.listen(PORT, () => {
 
   console.log('Port is working and listening  on port ' + PORT);
 });
+
 

0027b67af2a940dbd22f4da97f889d36ffa30f4a
gmireles210 <44076044+gmireles210@users.noreply.github.com>
2019-12-12 13:07:37 -0800

    updated js, sql, linter

--- schema.sql
+++ schema.sql
@@ -2,9 +2,10 @@ DROP TABLE IF EXISTS locations;
 
 CREATE TABLE locations (
    id SERIAL PRIMARY KEY,
+   /* get data from constructor function */
    search_query VARCHAR(255),
    formatted_query VARCHAR(255),
-   latitude NUMERIC (10, 7),
+   latitude NUMERIC (10, 7), /* can specify length allowed */
    longitude NUMERIC (10, 7)
 );
 

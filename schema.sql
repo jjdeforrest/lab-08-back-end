8e8c37b9a9148f43da934116010ea01d4cb0c320
gmireles210 <44076044+gmireles210@users.noreply.github.com>
2019-12-12 13:22:36 -0800

    update

--- schema.sql
+++ schema.sql
@@ -2,10 +2,9 @@ DROP TABLE IF EXISTS locations;
 
 CREATE TABLE locations (
    id SERIAL PRIMARY KEY,
-   /* get data from constructor function */
    search_query VARCHAR(255),
    formatted_query VARCHAR(255),
-   latitude NUMERIC (10, 7), /* can specify length allowed */
+   latitude NUMERIC (10, 7), 
    longitude NUMERIC (10, 7)
 );
 
